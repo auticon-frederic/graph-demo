458cf5bc9a1c3019dc2a91e1f7903300d09ee42e
auticon-frederic <frederic.armand.lhote@auticon-norway.com>
2025-08-26 11:41:10 +0000

    simplify code

--- gen-graph.py
+++ gen-graph.py
@@ -1,18 +1,13 @@
 #!/usr/bin/env python3
 
 import matplotlib.pyplot as plt
+import random
 
-# Sample data
-x = [1, 2, 3, 4,  5]
-y = [2, 4, 6, 8, 10]
+# Data
+x = list(range(30))
+y = list(map(lambda x: random.uniform(7,11), range(30)))
 
-# Create a plot
-plt.plot(x, y, marker="o")
-
-# Add labels and title
-plt.xlabel("X-axis")
-plt.ylabel("Y-axis")
-plt.title("Simple Line Chart")
-
-# Save as an image (PNG, JPG, SVG, etc.)
+# Plot
+plt.bar(x, y)
+plt.ylim(bottom=0)
 plt.savefig("chart.png")
